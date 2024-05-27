048ba8d35cba762ea260df14d04ab3b4327ca753
samirbenbouker <samirbenbouker@gmail.com>
2024-05-27 12:40:44 +0200

    comment code

--- contracts/MockNFT.sol
+++ contracts/MockNFT.sol
@@ -8,24 +8,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NFTDelegation.sol";
 
 contract MockNFT is ERC721, Ownable {
+    // Public variable to store the address of the NFTDelegation contract.
     address public nftDelegationContract;
+
+    // Public variable to store the instance of the NFTDelegation contract.
     NFTDelegation public nftDelegation;
 
     constructor(address _nftDelegationContract) ERC721("MockNFT", "MNFT") {
+        // Set the address of the NFTDelegation contract.
+        // Initialize the instance of the NFTDelegation contract.
         nftDelegationContract = _nftDelegationContract;
         nftDelegation = NFTDelegation(_nftDelegationContract);
     }
 
+    // Function to mint ERC721 tokens.
     function mint(address _to, uint256 _tokenId) external onlyOwner {
         _mint(_to, _tokenId);
     }
 
+    // Override the transferFrom function of ERC721 contract.
     function transferFrom(
         address _from,
         address _to,
         uint256 _tokenId
     ) public virtual override {
+        // Transfer the token.
         _transfer(_from, _to, _tokenId);
+
+        // Check if the receiver contract supports ERC721Receiver interface and handle the received token.
         require(
             IERC721Receiver(nftDelegationContract).onERC721Received(
                 msg.sender,
@@ -37,24 +47,31 @@ contract MockNFT is ERC721, Ownable {
         );
     }
 
+    // Internal function to handle token transfer.
     function _transfer(
         address _from,
         address _to,
         uint256 _tokenId
     ) internal override {
+        // Get the current owner of the token.
         address currentOwner = ownerOf(_tokenId);
         super._transfer(_from, _to, _tokenId);
+
+        // If the new owner is different from the current owner, revoke the delegation.
         if (currentOwner != _to) {
             revokeDelegation(_tokenId);
         }
     }
 
+    // Function to handle ERC721 token reception.
     function onERC721Received(
         address,
         address,
         uint256,
         bytes calldata
     ) external view returns (bytes4) {
+        // Ensure that the sender is the NFTDelegation contract.
+        // Return the ERC721Received selector.
         require(
             _msgSender() == nftDelegationContract,
             "Can only receive from NFTDelegation contract"
@@ -62,8 +79,12 @@ contract MockNFT is ERC721, Ownable {
         return IERC721Receiver.onERC721Received.selector;
     }
 
+    // Internal function to revoke delegation of an NFT.
     function revokeDelegation(uint256 _tokenId) internal {
+        // Get the delegate address for the token.
         address delegate = nftDelegation.getDelegate(_tokenId);
+
+        // Revoke the delegation if delegate is not zero address.
         if (delegate != address(0)) {
             nftDelegation.revokeDelegation(_tokenId);
         }
--- contracts/NFTDelegation.sol
+++ contracts/NFTDelegation.sol
@@ -1,60 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Imports interfaces for ERC721 token and ERC721 receiver.
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract NFTDelegation is IERC721Receiver {
+    // Mapping to store delegate addresses for each token ID.
     mapping(uint256 => address) private delegates;
+
+    // Reference to the ERC721 contract instance.
     IERC721 private nftContract;
 
+    // Event emitted when delegation is made.
     event Delegated(
         address indexed owner,
         address indexed delegate,
         uint256 indexed tokenId
     );
+
+    // Event emitted when delegation is revoked.
     event Revoked(address indexed owner, uint256 indexed tokenId);
 
     constructor() {}
 
+    // Function to delegate an NFT to another address.
     function delegateNFT(address _delegate, uint256 _tokenId) public {
+        // Get the current owner of the NFT
         address owner = nftContract.ownerOf(_tokenId);
+
+        // Ensure that the caller is the owner of the NFT
+        // Ensure that the delegate address is valid
         require(owner == msg.sender, "You are not the owner of this NFT");
         require(_delegate != address(0), "Invalid delegate address");
 
+        // Get the current delegate for the tokenId
+        // Set the new delegate for the tokenId
         address currentDelegate = delegates[_tokenId];
         delegates[_tokenId] = _delegate;
 
+        // Emit event if there was a previous delegate
         if (currentDelegate != address(0)) {
             emit Revoked(owner, _tokenId);
         }
+
         emit Delegated(owner, _delegate, _tokenId);
     }
 
+    // Function to revoke delegation of an NFT
     function revokeDelegation(uint256 _tokenId) public {
         address owner = nftContract.ownerOf(_tokenId);
+
+        // Remove the delegate for the tokenId
         delete delegates[_tokenId];
         emit Revoked(owner, _tokenId);
     }
 
+    // Function to get the delegate address for a token ID.
     function getDelegate(uint256 _tokenId) public view returns (address) {
         return delegates[_tokenId];
     }
 
+    // Function to handle ERC721 token reception.
     function onERC721Received(
         address,
         address,
         uint256 _tokenId,
         bytes calldata
     ) external override returns (bytes4) {
+        // Ensure that the sender is the NFT contract
         require(
             msg.sender == address(nftContract),
             "Only allowed from the NFT contract"
         );
+
+        // Revoke delegation for the receiver tokenId
+        // Return the Recevied selector
         revokeDelegation(_tokenId);
         return this.onERC721Received.selector;
     }
 
+    // Function to set the ERC721 contract address.
     function setNFTContract(address _nftContractAddress) public {
         nftContract = IERC721(_nftContractAddress);
     }
